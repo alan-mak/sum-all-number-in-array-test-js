b2a3e3b5ed3c7bd5bf157d8fc18d53626060cf8a
Alan <alanmak95@gmail.com>
2020-12-04 03:46:11 +0000

    Solved sum with unlimited array

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+  for(let number of array) {
+    if(Array.isArray(number)) {
+      number = sumItems(number);
+    }
+    sum += number;
+  }
+  return sum;
 }
-
 module.exports = sumItems;
+
+sumItems([1, [3, 3], 5, 5])
